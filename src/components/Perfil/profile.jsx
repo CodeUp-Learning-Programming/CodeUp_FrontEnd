26ce32f16231801c2ff2c169ae0ed722598cdb40
Rostichelli <douglas.alves@sptech.school>
2024-04-09 21:11:03 -0300

    Add drop-box + add foto de perfil

--- src/components/Perfil/profile.jsx
+++ src/components/Perfil/profile.jsx
@@ -16,13 +16,13 @@ function UserDropdownMenu() {
   return (
     <div className="user-dropdown">
       <button onClick={toggleMenu} className="user-dropdown-toggle">
-        <img src="img.avatar"  className="avatar" />
+        <img src={sessionStorage.fotoPerfil}  className="avatar" />
       </button>
       {isOpen && (
         <div className="user-dropdown-menu">
           <ul>
             <li><a href="/perfil">Profile</a></li>
-            <li><a href="#" onClick={handleLogout}>Sign Out</a></li>
+            <li><a href="/login" onClick={handleLogout}>Sign Out</a></li>
             {/* Adicione outras opções de menu aqui, se necessário */}
           </ul>
         </div>
